a2be120719a03add8f53b3cbeacbdcab1b3be6c9
MarkDucommun <ducommun.mark@gmail.com>
2013-10-15 00:01:23 -0500

    additional refactors to js

--- sudoku.js
+++ sudoku.js
@@ -1,52 +1,25 @@
 function Sudoku(puzzle){
+  var start_time = new Date().getTime();
   this.puzzle = puzzle;
   this.prepare();
-  return this.solve();
+  this.solve();
+  this.time = new Date().getTime() - start_time;
 }
 
 Sudoku.prototype.prepare = function() {
-
-  this.index_methods = [foo, baz, bar]
-
+  this.index_methods = [get_row_index, get_column_index, get_box_index]
   this.puzzle = this.puzzle.split("");
-
   this.size = this.puzzle.length;
-
   this.box_size = Math.sqrt(this.size);
-  
   this.set_sum = get_set_sum(this.box_size);
-
   this.set_possible();
-
   this.previous_state = [];
-}
-
-var foo = function(cell_index, box_size) {
-  return Math.floor(cell_index / box_size);
-}
-
-var baz = function(cell_index, box_size) {
-  return cell_index % box_size;
-}
-
-var bar = function(cell_index, box_size) {
-  var box_dimension = Math.sqrt(box_size);
-  var box_row_index = Math.floor(foo(cell_index, box_size) / box_dimension);
-  var box_column_index = Math.floor(baz(cell_index, box_size) / box_dimension);
-  return box_row_index * box_dimension + box_column_index;
-}
-
-Sudoku.prototype.set_possible = function(){
-  for (var i = 0; i < this.size; i++) {
-    if (this.puzzle[i] == "0") {
-      this.puzzle[i] = get_possibility_array(this.box_size);
-    }
-  }
 }
 
 Sudoku.prototype.solve = function() {
   var changed = true;
   var unsolved = true;
+  var previous_state = [];
 
   while (unsolved && changed) {
     for (var i = 0; i < this.size; i++) {
@@ -55,12 +28,13 @@ Sudoku.prototype.solve = function() {
       }
     }
     unsolved = !this.solved();
-    changed = this.changed();
+    changed = !array_equals(this.previous_state, this.puzzle);
     this.previous_state = copy_array(this.puzzle);
   }
 
   if (unsolved) {
     var unsolved_cell = this.first_unsolved_cell();
+  
     for (var i = 0; i < this.puzzle[unsolved_cell].length && unsolved; i++) {
       var copy = copy_array(this.puzzle);    
       copy[unsolved_cell] = this.puzzle[unsolved_cell][i];
@@ -71,26 +45,12 @@ Sudoku.prototype.solve = function() {
         this.puzzle = copy.puzzle;
       }
     }
-  }
-
-  return this; 
-}
-
-Sudoku.prototype.first_unsolved_cell = function() {
-  for (var i = 0; i < this.size; i++) {
-    if (this.puzzle[i] instanceof Array) {
-      return i;
-    }
-  }
-  return -1;
-}
-
-Sudoku.prototype.changed = function() {
-  return !array_equals(this.previous_state, this.puzzle);
+  } 
 }
 
 Sudoku.prototype.solve_cell = function(cell_index) {
   var possible = this.eliminate_final_values(cell_index);
+  
   if (possible.length > 1) {
     possible = this.eliminate_possible_values(cell_index);
   }
@@ -98,41 +58,10 @@ Sudoku.prototype.solve_cell = function(cell_index) {
   return possible;
 }
 
-Sudoku.prototype.eliminate_final_values = function(cell_index) {
-  var possible = this.puzzle[cell_index];
-
-  possible = remove_possibilities(possible, this.get_final_set(cell_index, foo));
-  possible = remove_possibilities(possible, this.get_final_set(cell_index, baz)); 
-  possible = remove_possibilities(possible, this.get_final_set(cell_index, bar));
-
-  return possible;  
-}
-
-Sudoku.prototype.eliminate_possible_values = function(cell_index) {
-  var possible = [];
-
-  possible.push(copy_array(this.puzzle[cell_index]));
-  possible.push(copy_array(this.puzzle[cell_index]));
-  possible.push(copy_array(this.puzzle[cell_index]));
-
-  possible[0] = remove_possibilities(possible[0], this.get_possible_set(cell_index, foo));
-  possible[1] = remove_possibilities(possible[1], this.get_possible_set(cell_index, baz));
-  possible[2] = remove_possibilities(possible[2], this.get_possible_set(cell_index, bar));
-
-  var final = this.puzzle[cell_index];
-  for (var i = 0; i < 3; i++) {
-    if (possible[i].length > 0 && possible[i].length < final.length) {
-      final = possible[i];
-    }
-  }
-  return final;
-}
-
 Sudoku.prototype.solved = function() {
-  var methods = [foo, baz, bar];
   for (var j = 0; j < 3; j++) {
     for (var i = 0; i < this.size; i = i + this.box_size) {
-      var set = this.get_final_set(i, methods[j]);
+      var set = this.get_final_set(i, this.index_methods[j]);
       if (!this.set_solved(set)) {
         return false;
       }
@@ -143,10 +72,20 @@ Sudoku.prototype.solved = function() {
 
 Sudoku.prototype.set_solved = function(set) {
   var sum = 0;
+
   for (var i = 0; i < set.length; i++) {
     sum += parseInt(set[i]);
   }
   return sum == this.set_sum;
+}
+
+Sudoku.prototype.eliminate_final_values = function(cell_index) {
+  var possible = this.puzzle[cell_index];
+
+  for (var i = 0; i < 3; i++) {
+    possible = remove_possibilities(possible, this.get_final_set(cell_index, this.index_methods[i]));
+  }
+  return possible;  
 }
 
 Sudoku.prototype.get_final_set = function(cell_index, index_method) {
@@ -161,6 +100,22 @@ Sudoku.prototype.get_final_set = function(cell_index, index_method) {
   return set;
 }
 
+Sudoku.prototype.eliminate_possible_values = function(cell_index) {
+  var possible = [];
+  var final = this.puzzle[cell_index];
+
+  for (var i = 0; i < 3; i++) {
+    possible[i] = remove_possibilities(copy_array(this.puzzle[cell_index]), this.get_possible_set(cell_index, this.index_methods[i]));
+  }
+
+  for (var i = 0; i < 3; i++) {
+    if (possible[i].length > 0 && possible[i].length < final.length) {
+      final = possible[i];
+    }
+  }
+  return final;
+}
+
 Sudoku.prototype.get_possible_set = function(cell_index, index_method) {
   var set = [];
   var set_index = index_method(cell_index, this.box_size);
@@ -173,8 +128,26 @@ Sudoku.prototype.get_possible_set = function(cell_index, index_method) {
   return flatten(set); 
 }
 
+Sudoku.prototype.first_unsolved_cell = function() {
+  for (var i = 0; i < this.size; i++) {
+    if (this.puzzle[i] instanceof Array) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+Sudoku.prototype.set_possible = function(){
+  for (var i = 0; i < this.size; i++) {
+    if (this.puzzle[i] == "0") {
+      this.puzzle[i] = create_possibilities(this.box_size);
+    }
+  }
+}
+
 Sudoku.prototype.print = function(){
   var string = "";
+  
   for (var i = 0; i < this.size; i++){
     if (i % this.box_size == 0){
       string += "\n\n";
@@ -182,7 +155,47 @@ Sudoku.prototype.print = function(){
     string += " " + this.puzzle[i] + " ";
   }
   console.log(string);
-  console.log("");
+  console.log("\n" + this.time + "ms to solve.");
+}
+
+// Utility methods
+
+var get_row_index = function(cell_index, box_size) {
+  return Math.floor(cell_index / box_size);
+}
+
+var get_column_index = function(cell_index, box_size) {
+  return cell_index % box_size;
+}
+
+var get_box_index = function(cell_index, box_size) {
+  var box_dimension = Math.sqrt(box_size);
+  var box_row_index = Math.floor(get_row_index(cell_index, box_size) / box_dimension);
+  var box_column_index = Math.floor(get_column_index(cell_index, box_size) / box_dimension);
+  return box_row_index * box_dimension + box_column_index;
+}
+
+var get_set_sum = function(num) {
+  var sum = 0;
+  for (var i = 1; i <= num; i++){
+    sum += i;
+  }
+  return sum;
+}
+
+var create_possibilities = function(num) {
+  var possible = "";
+  for (var i = 1; i <= num; i++) {
+    possible += i
+  }
+  return possible.split(""); 
+}
+
+var remove_possibilities = function(possible, set) {
+  for (var i = 0; i < set.length; i++) {
+    remove(possible, set[i]);
+  }
+  return possible;
 }
 
 var array_to_string = function(array) {
@@ -194,31 +207,11 @@ var array_to_string = function(array) {
   return array.join("");
 }
 
-var get_set_sum = function(num) {
-  var sum = 0;
-  for (var i = 1; i <= num; i++){
-    sum += i;
-  }
-  return sum;
-}
-
-var get_possibility_array = function(num) {
-  var possible = "";
-  for (var i = 1; i <= num; i++) {
-    possible += i
-  } 
-  return possible.split(""); 
-}
-
-var remove_possibilities = function(possible, set) {
-  for (var i = 0; i < set.length; i++) {
-    remove(possible, set[i]);
-  }
-  return possible;
-}
+// Array methods
 
 var copy_array = function(array) {
   var copy = [];
+  
   for(var i = 0; i < array.length; i++) {
     copy.push(array[i]);
   }
@@ -268,9 +261,10 @@ var remove = function(array, element) {
   return -1;
 }
 
+// Driver code
+
 new Sudoku("0003040000201000").print();
 new Sudoku("0004000000100100").print();
-
 new Sudoku("105802000090076405200400819019007306762083090000061050007600030430020501600308900").print();
 new Sudoku("005030081902850060600004050007402830349760005008300490150087002090000600026049503").print();
 new Sudoku("290500007700000400004738012902003064800050070500067200309004005000080700087005109").print();
